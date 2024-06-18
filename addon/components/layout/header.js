ba42907eb3b8ab848d806dfa7b77bf4fe83291bc
Mukira <mukira@lipagas.co.ke>
2024-06-18 08:21:38 +0000

    first commit

--- addon/components/layout/header.js
+++ addon/components/layout/header.js
@@ -170,24 +170,24 @@ export default class LayoutHeaderComponent extends Component {
         }
 
         items.pushObjects([
-            {
-                href: 'https://discord.gg/MJQgxHwN',
-                target: '_discord',
-                text: 'Join Discord Community',
-                icon: 'arrow-up-right-from-square',
-            },
-            {
-                href: 'https://github.com/fleetbase/fleetbase/issues',
-                target: '_support',
-                text: 'Help & Support',
-                icon: 'arrow-up-right-from-square',
-            },
-            {
-                href: 'https://fleetbase.github.io/api-reference/',
-                target: '_api',
-                text: 'API Reference',
-                icon: 'arrow-up-right-from-square',
-            },
+            // {
+            //     href: 'https://discord.gg/MJQgxHwN',
+            //     target: '_discord',
+            //     text: 'Join Discord Community',
+            //     icon: 'arrow-up-right-from-square',
+            // },
+            // {
+            //     href: 'https://github.com/fleetbase/fleetbase/issues',
+            //     target: '_support',
+            //     text: 'Help & Support',
+            //     icon: 'arrow-up-right-from-square',
+            // },
+            // {
+            //     href: 'https://fleetbase.github.io/api-reference/',
+            //     target: '_api',
+            //     text: 'API Reference',
+            //     icon: 'arrow-up-right-from-square',
+            // },
             {
                 component: 'layout/header/dark-mode-toggle',
             },
